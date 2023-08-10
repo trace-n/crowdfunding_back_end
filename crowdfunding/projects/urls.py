5a416182e569985094e45ccce563c5f3c10ee1de
Tracey Nguyen <trace_n@hotmail.com>
2023-08-10 23:15:46 +1000

    updated url for stats

--- crowdfunding/projects/urls.py
+++ crowdfunding/projects/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('projects/<int:pk>/', views.ProjectDetail.as_view()),
     path('pledges/', views.PledgeList.as_view()),  
     path('pledges/<int:pk>/', views.PledgeDetail.as_view()),  
-    path('projects/statistics', views.ProjectStatistics.as_view()),  
+    path('projects/statistics/', views.ProjectStatistics.as_view()),  
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
